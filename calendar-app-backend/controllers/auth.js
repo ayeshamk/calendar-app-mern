71898e1dd9c4e68243fa49b08507783f1844d2dd
ayeshamk <ayesha.malika.khan@gmail.com>
2021-11-02 01:57:20 +0400

    auth register modification

--- calendar-app-backend/controllers/auth.js
+++ calendar-app-backend/controllers/auth.js
@@ -7,7 +7,7 @@ export const register = async (req, res) => {
     const { name, email, password } = req.body;
     // validation
     if (!name) return res.status(400).send("Name is required");
-    if (!password || password.length < 6)
+    if (!password || password.length < 5)
       return res
         .status(400)
         .send("Password is required and should be min 6 characters long");
